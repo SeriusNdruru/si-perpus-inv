682753d9b03f7ccf1e221c438d0fe13a4be6a2d3
SeriusNdruru <seriusndruru7@gmail.com>
2026-08-06 18:47:17 +0700

    Initial commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -18,7 +18,7 @@
     @endphp
     <title>@yield('title', 'Dashboard') | {{ $browserSystemName }}</title>
     @include('shared.favicon-links')
-    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v=110">
+    <link rel="stylesheet" href="{{ asset('css/app.css') }}?v=111">
 </head>
 <body class="app-page">
     @php
